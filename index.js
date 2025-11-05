3e5afaf7c3ef7be1cc046863cc42d5eaf0df81cf
prodigall.me <and020702@gmail.com>
2025-11-05 20:48:15 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import bodyParser from "body-parser";
+
+import { createApp } from "./app.js";
+
+const app = createApp(express, bodyParser, createReadStream, crypto, http);
+
+app.listen(80);
